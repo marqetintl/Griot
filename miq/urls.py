c306fa8efe650506764d694b03dfa19b6077835a
Michael <michaelgainyo@gmail.com>
2021-05-11 22:30:16 -0400

    Added grio-admin

--- miq/urls.py
+++ miq/urls.py
@@ -1,9 +1,10 @@
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 
 from rest_framework import routers
 
 from .views import PageView, IndexView
+from .auth.views import GrioAdminView
 from .auth.viewsets import SectionViewset, PageViewset, IndexViewset
 
 app_name = 'miq'
@@ -14,8 +15,17 @@ auth_router.register(r'sections', SectionViewset)
 auth_router.register(r'index', IndexViewset)
 
 
+admin_views = [
+
+    # Catch-all url
+    re_path(r'', GrioAdminView.as_view(), name='grio-index'),
+]
+
+
 urlpatterns = [
     path(f'{settings.API_PATH}/', include(auth_router.urls)),
+
+    path('grio/', include((admin_views))),
 
     path('<slug:slug>/', PageView.as_view(), name='page'),
     path('', IndexView.as_view(), name='index'),
