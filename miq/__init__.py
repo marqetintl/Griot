f62c3a5e552d416ee342d12a4b5c5823f82274cc
Michaël <michael.gainyo@gmail.com>
2021-06-09 01:42:47 -0400

    Updated miq

--- miq/__init__.py
+++ miq/__init__.py
@@ -1,9 +1,9 @@
+
 """
-Core Models
+Core
 """
 
 __author__ = "Michaël Gainyo"
 __email__ = "michaelgainyo@gmail.com"
 __version__ = "1.0.0"
 __copyright__ = "Copyright (C) 2021 Michaël Gainyo and contributors"
-__license__ = "MIT"
